5135db58ffeec8d951aa966f1acb0dc1812e0847
samhomesss <159544864+samhomesss@users.noreply.github.com>
2025-08-29 17:24:30 +0900

    [FEAT] DamageUp

--- Assets/@Scripts/Contents/UI/ContentsUI/UI_EnemyDamageUp.cs
+++ Assets/@Scripts/Contents/UI/ContentsUI/UI_EnemyDamageUp.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI_EnemyDamageUp : MonoBehaviour
+public class UI_EnemyDamageUp : UI_Scene
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    enum GameObjects
     {
-        
+        DamageUpCoolTimer,
+        CoolTimer,
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject _damageUpCoolTimer;
+    Image _coolTimer;
+
+    public override bool Init()
     {
-        
+        if (base.Init() == false)
+            return false;
+
+        BindObjects(typeof(GameObjects));
+
+        _damageUpCoolTimer = GetObject((int)GameObjects.DamageUpCoolTimer);
+        _coolTimer = GetObject((int)GameObjects.CoolTimer).GetComponent<Image>();
+
+        _damageUpCoolTimer.SetActive(false);
+
+        return true;
     }
+
+    //  만들어야 하는거 DamageUpItem을 먹으면 DamageUpCoolTimer 오브젝트를 활성화 시키고 DamageUPItem의 duration 만큼 CoolTimer의
+    //  Fill을 감소시킨다 해당 Fill이 0이 되면 DamageUpCoolTimer 오브젝트를 비활성화
 }
--- Assets/@Scripts/Contents/UI/ContentsUI/UI_PlayerDamageUp.cs
+++ Assets/@Scripts/Contents/UI/ContentsUI/UI_PlayerDamageUp.cs
@@ -1,16 +1,31 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UI_PlayerDamageUp : MonoBehaviour
+public class UI_PlayerDamageUp : UI_Scene
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    enum GameObjects
     {
-        
+        DamageUpCoolTimer,
+        CoolTimer,
     }
 
-    // Update is called once per frame
-    void Update()
+    GameObject _damageUpCoolTimer;
+    Image _coolTimer;
+
+    public override bool Init()
     {
-        
+        if (base.Init() == false)
+            return false;
+
+        BindObjects(typeof(GameObjects));
+
+        _damageUpCoolTimer = GetObject((int)GameObjects.DamageUpCoolTimer);
+        _coolTimer = GetObject((int)GameObjects.CoolTimer).GetComponent<Image>();
+
+        _damageUpCoolTimer.SetActive(false);
+
+        return true;
     }
+    //  만들어야 하는거 DamageUpItem을 먹으면 DamageUpCoolTimer 오브젝트를 활성화 시키고 DamageUPItem의 duration 만큼 CoolTimer의
+    //  Fill을 감소시킨다 해당 Fill이 0이 되면 DamageUpCoolTimer 오브젝트를 비활성화
 }
