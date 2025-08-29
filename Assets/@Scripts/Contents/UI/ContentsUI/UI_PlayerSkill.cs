37715bb71f4693411af9f6631321e51a8fc34593
samhomesss <159544864+samhomesss@users.noreply.github.com>
2025-08-29 14:40:31 +0900

    [FEAT] UI_Skills

--- Assets/@Scripts/Contents/UI/ContentsUI/UI_PlayerSkill.cs
+++ Assets/@Scripts/Contents/UI/ContentsUI/UI_PlayerSkill.cs
@@ -35,6 +35,8 @@ public class UI_PlayerSkill : UI_Scene
         BindButtons(typeof(Buttons));
         BindObjects(typeof(GameObjects));
 
+        // 처음 시작 초기화 부분 공통된 부분이 많아서 합쳐줘
+
         _skill1Cool = GetObject((int)GameObjects.Skill1CoolTimer);
         _skill2Cool = GetObject((int)GameObjects.Skill2CoolTimer);
         _skill3Cool = GetObject((int)GameObjects.Skill3CoolTimer);
@@ -47,40 +49,42 @@ public class UI_PlayerSkill : UI_Scene
         _skill4Cool.SetActive(false);
         _skill5Cool.SetActive(false);
 
-        GetButton((int)Buttons.SkillCardImage1).onClick.AddListener(() => 
+        return true;
+    }
+
+    private void Start()
+    {
+        GetButton((int)Buttons.SkillCardImage1).onClick.AddListener(() =>
         {
             // 플레이어 SkillJumpShot 실행
             // 실행했을때 해당 쿨 타임 만틈 Skill1CoolTimer 켰다가 끄기 
             // SKill1CoolTimer 자식에 있는 TMP_Text를 통해서 쿨타임이 얼마나 남았는지 알려주기 
         });
 
-        GetButton((int)Buttons.SkillCardImage2).onClick.AddListener(() => 
+        GetButton((int)Buttons.SkillCardImage2).onClick.AddListener(() =>
         {
             // 플레이어 SkillVerTicalRainStrike 실행
             // 실행했을때 해당 쿨 타임 만틈 Skill2CoolTimer 켰다가 끄기 
             // SKill2CoolTimer 자식에 있는 TMP_Text를 통해서 쿨타임이 얼마나 남았는지 알려주기 
         });
 
-        GetButton((int)Buttons.SkillCardImage3).onClick.AddListener(() => 
+        GetButton((int)Buttons.SkillCardImage3).onClick.AddListener(() =>
         {
             // 플레이어 SkillPiercingShoot 실행
             // 실행했을때 해당 쿨 타임 만틈 Skill3CoolTimer 켰다가 끄기 
             // SKill3CoolTimer 자식에 있는 TMP_Text를 통해서 쿨타임이 얼마나 남았는지 알려주기 
         });
-        GetButton((int)Buttons.SkillCardImage4).onClick.AddListener(() => 
+        GetButton((int)Buttons.SkillCardImage4).onClick.AddListener(() =>
         {
             // 플레이어 SkillStunShoot 실행
             // 실행했을때 해당 쿨 타임 만틈 Skill4CoolTimer 켰다가 끄기 
             // SKill4CoolTimer 자식에 있는 TMP_Text를 통해서 쿨타임이 얼마나 남았는지 알려주기 
         });
-        GetButton((int)Buttons.SkillCardImage5).onClick.AddListener(() => 
+        GetButton((int)Buttons.SkillCardImage5).onClick.AddListener(() =>
         {
             // 플레이어 SkillDash 실행
             // 실행했을때 해당 쿨 타임 만틈 Skill5CoolTimer 켰다가 끄기 
             // SKill5CoolTimer 자식에 있는 TMP_Text를 통해서 쿨타임이 얼마나 남았는지 알려주기 
         });
-
-
-        return true;
     }
 }
